fe3561fc90f6fe08cfff71bf73801757384b1e30
nagisettyramu007@gmail.com <nagisettyramu007@gmail.com>
2020-09-28 00:11:05 +0530

    server chnages

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 
 const app = express();
+app.use(express.static('dist'));
 
 app.listen(process.env.PORT || 8000, () => console.log(`Listening on port ${process.env.PORT || 8000}!`));
