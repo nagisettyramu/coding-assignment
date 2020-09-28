ca7d9a26c20076b381d9d50137baee966cc25db8
nagisettyramu007@gmail.com <nagisettyramu007@gmail.com>
2020-09-28 07:17:34 +0530

    build issues fixes

---
+++ build/precache-manifest.c6627f128c65b01f8f38cc6ed0c1cc77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33d80a7df368dc41f4cef7f940fbeae3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1d5378a8548da07b52f",
+    "url": "/static/css/main.46533812.chunk.css"
+  },
+  {
+    "revision": "af1399fe9ee621a8d3a9",
+    "url": "/static/js/2.ae520f57.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ae520f57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1d5378a8548da07b52f",
+    "url": "/static/js/main.f539a47d.chunk.js"
+  },
+  {
+    "revision": "9c2677f05060f1b94868",
+    "url": "/static/js/runtime-main.aff08fdb.js"
+  }
+]);
--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
 const app = express();
-app.use(express.static('dist'));
+app.use(express.static('build'));
 
 app.listen(process.env.PORT || 8000, () => console.log(`Listening on port ${process.env.PORT || 8000}!`));
